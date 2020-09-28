ac32862b3eb1f08d06b77abca8a1eb351d307830
Santiago Gomez <santiagogomezl@Santiagos-MacBook-Pro.local>
2020-09-28 07:29:20 -0700

    Server API config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'developmment'
+    NODE_ENV: process.env.NODE_ENV || 'developmment',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost/'
 }
