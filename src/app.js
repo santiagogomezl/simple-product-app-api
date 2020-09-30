0fa918f45d374ebad1f667a71eb5b9b087f8ff0e
Santiago Gomez <santiagogomezl@Santiagos-MacBook-Pro.local>
2020-09-30 08:30:42 -0700

    API AuthoAuthorization Token setup

--- src/app.js
+++ src/app.js
@@ -2,24 +2,30 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config');
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const productsRouter = require('../products/products-router.js')
-
 
 const app = express()
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test'
 }))
-app.use(cors())
+app.use(cors({origin: CLIENT_ORIGIN}))
 app.use(helmet())
 
-app.use('/api/products', productsRouter)
-
-app.get('/api', (req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {  
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
 })
+
+app.use('/api/products', productsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
   let response
--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'developmment',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://localhost/',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/simple-product',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
 
 }
